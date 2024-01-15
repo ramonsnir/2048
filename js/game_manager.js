a026a22d10e0a84acc785e06776841218b09ae8a
Ramon Snir <r@mon.dev>
2024-01-15 11:14:19 -0500

    Disable "winning" after 2048

--- js/game_manager.js
+++ js/game_manager.js
@@ -166,8 +166,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // REMOVED: The mighty 2048 tile
+          // if (merged.value === 2048) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
